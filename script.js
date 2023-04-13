d4a441a5b401df3cce9264d2dabb664d16cc26c9
Jhonny Mosquera Ocampo <ebusiness.latino@live.com>
2023-04-13 09:16:00 +0100

    API_key

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ inputQuestion.addEventListener("keypress", (e) => {
     if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY = "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";  
+const OPENAI_API_KEY = "";  
 
 function SendQuestion() {
     var sQuestion = inputQuestion.value;
